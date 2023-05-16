5593eb7d9183bdf0f5d2fa2049b4ee7fb65920c3
Leonid Melnichenko <melnichenko.leon@gmail.com>
2023-05-16 16:56:26 +0300

    successful test

--- src/test/java/by/itacademy/melnichenko/CWTest.java
+++ src/test/java/by/itacademy/melnichenko/CWTest.java
@@ -3,7 +3,6 @@ package by.itacademy.melnichenko;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
